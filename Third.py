ab2c8e16eed7e68b68d6bf81b9339ab5f282c1e4
Vadaliya Falsi <110164717+Falsi3007@users.noreply.github.com>
2025-01-10 18:42:33 +0530

    Create Third.py

---
+++ Third.py
@@ -0,0 +1,50 @@
+# marks = [23,45,56,67,88]
+# print(marks)
+# print(len(marks))
+# print(marks[3])
+
+
+#Tuple
+# tup = (33,45,77,35)        #not change the value of tuple
+# print(type(tup))
+
+# tup = (33)        #only num that mean int if we 33, that's the tuple
+# print(type(tup))
+
+# tup = (1,2,3,4,2)
+# print(tup[1:4])
+# print(tup.index(3))
+# print(tup.count(2))
+
+
+#practice
+# a = input("Enter 1st movie:")
+# b = input("Enter 2nd movie:")
+# c = input("Enter 3rd movie:")
+# list = [a,b,c]
+# print(list)
+
+# mov=[]
+# a = input("Enter 1st movie:")
+# b = input("Enter 2nd movie:")
+# c = input("Enter 3rd movie:")
+# mov.append(a)               #also we directly append the movies
+# mov.append(b)
+# mov.append(c)
+# print(mov)
+
+
+# list = [1,2,3]
+# copy_list = list.copy()
+# copy_list.reverse()
+# if(copy_list == list):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+# tup = ("c","b","a","b","d","a","c","a")
+# print(tup.count("b"))
+
+# list = ["c","b","a","b","d","a","c","a"]
+# list.sort()
+# print(list)
