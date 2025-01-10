efffee683c3e4ab0eb49321c694d1882b7c65d38
Vadaliya Falsi <110164717+Falsi3007@users.noreply.github.com>
2025-01-10 18:41:41 +0530

    Create FirstProgram.py

---
+++ FirstProgram.py
@@ -0,0 +1,90 @@
+# print("hi")
+
+# #arithmetic operator
+
+# a = 10
+# b = 5
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+
+# #relational operator
+
+# a = 50
+# b = 20
+
+# print (a==b)
+# print (a!=b)
+# print (a>b)
+# print (a<=b)
+
+# #assignment operator
+
+# num = 10
+# num **= 5
+# print(num)
+
+# #logical operator
+
+
+# a = 3
+# b = 2
+# print (not False)
+# print(not (a>b))
+
+# val1 = True 
+# val2 = False
+# print (val1 and val2)
+
+# print(val1 or val2)  #also use the expression
+
+
+# #Type conversion
+# a = 2 
+# b = 4.25
+# print(a+b)   #automatically conversion 
+
+# a = int("2")        #Type casting 
+# b = 4.25
+# print(a+b)
+
+
+# a = 3.14
+# a = str(a)
+# print(type(a))
+
+
+# #input in python
+
+# a  = input("write your name : ")
+# print(a)
+
+# name = input("Enter name: ")
+# age = int(input("Enter Age: "))
+# marks = float(input("Enter marks: "))
+# print("Welcome ",name)
+# print("Age: ",age)
+# print("marks: ",marks)
+
+
+#practise
+
+# a = int(input("Enter First:"))
+# b = int(input("Enter Second:"))
+# print (a+b)
+
+# side = float(input("Enter the side:"))
+# print(side*side)
+
+# a = float(input("Enter First:"))
+# b = float(input("Enter Second:"))
+# print((a+b)/2)
+
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+print(a>=b) 
