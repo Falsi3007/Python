f4189a59415718aaa19e3bdeebaccc6bbb46e137
Vadaliya Falsi <110164717+Falsi3007@users.noreply.github.com>
2025-01-10 18:42:04 +0530

    Create Second.py

---
+++ Second.py
@@ -0,0 +1,157 @@
+# student = ["Falsi", 3, "aayu", 45]
+# print(student[1])      #if we access list out of length that's get error
+# student[2] = "pichu"
+# print(student)
+ 
+# number = [23,45,77,18,65,55]
+# print(number[3:6])
+# print(number[-5:-1])
+ 
+#method of listing
+# num = [2,3,4]
+# print(num.append(5))
+# print(num.sort())
+# print(num.reverse())
+# print(num.insert(1,9))
+# print(num)  
+ 
+# name = ["apple","litchi","banana"]      #also use for char
+# name.sort()
+# print(name)
+
+#after two methods pop ....
+
+# str1 = "Hello \tWorld"
+# print(str1)
+
+# #length
+# str1 = "Hello"
+# str2 = "World"
+# final_str=str1+" "+str2
+
+# print(final_str)
+# print(len(final_str))
+
+
+# #indexing 
+# str = "Apna College"
+# print(str[5])
+
+
+# slicing
+# str = "Apna College"
+# print(str[:4])   #[0:4]
+# print(str[5:])   #[5:12]
+# print(str[3:8])
+
+
+# str1 = "apple"
+# print(str1[-3:-1])
+
+
+#diff function
+str = "i am learning python."
+# print(str.endswith("on."))
+
+# str = str.capitalize()
+# print(str)
+
+# print(str.replace("a","z"))
+
+# print(str.find("python"))
+# print(str.find("x"))
+
+# print(str.count("am"))
+# print(str.count("a"))
+
+
+#practise
+
+# str = input("Enter name:")
+# print(str, len(str))
+
+# str = "i am the $ Symbol of $$"
+# print(str.count("$"))
+
+
+#conditional statements
+
+# light = "green"
+# if(light == "red"):
+#     print("wait")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("look")
+# print("end of code")
+
+# num =  5
+# if(num>2):
+#     print("greater than 2")
+# if(num>3):
+#     print("greater than 3")
+
+# num =  5
+# if(num>2):
+#     print("greater than 2")
+# elif(num>3):
+#     print("greater than 3")
+
+# age = 20
+# if(age>=18):
+#     print("can vote")
+# else:
+#     print("can't vote")
+
+# marks = float(input("Enter the marks:"))
+# if(marks >= 90):
+#     print("Grade A")
+# elif(marks < 90 and marks>=80):
+#     print("Grade B")
+# elif(marks < 80 and marks>=70):
+#     print("Grade C")
+# else:
+#     print("Grade D")
+
+# marks = float(input("Enter the marks:"))
+# if(marks >= 90):
+#     Grade = "A"
+# elif(marks < 90 and marks>=80):
+#     Grade = "B"
+# elif(marks < 80 and marks>=70):
+#     Grade = "C"
+# else:
+#     Grade = "D"
+# print("Grade of Student ",Grade)
+
+# age = 15
+# if(age >= 18):
+#     if(age >= 80):
+#         print("can't drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("can't drive")
+
+#practise 
+# num = int(input("Enter the num: "))
+# if(num%2 == 0):
+#     print("even")
+# else:
+#     print("odd")
+
+# a = 400
+# b = 5
+# c = 10
+# if(a>b and a>c):
+#     print("A is greater")
+# elif(b>c):
+#     print("B is greater")
+# else:
+#     print("C is greater")
+
+# num = int(input("Enter the number:"))
+# if(num%7 == 0):
+#     print("multiple of 7")
+# else:
+#     print("not multiple of 7")
