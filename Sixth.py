a4d514d38f6c99881a0a027b269f14196ea5b4d4
Vadaliya Falsi <110164717+Falsi3007@users.noreply.github.com>
2025-01-10 18:44:06 +0530

    Create Sixth.py

---
+++ Sixth.py
@@ -0,0 +1,93 @@
+# count=1
+# while count<=10:
+#     print("hello")
+#     count += 1
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+
+# i=5
+# while i>=1:
+#     print(i)
+#     i-=1
+
+#practice
+# i=1
+# while i<=100:
+#     print(i)
+#     i+=1
+
+# i=100
+# while i>=1:
+#     print(i)
+#     i-=1
+
+# x = int(input("enter the number: "))
+# i=1
+# while i<=10:
+#     print(x, "*", i, "="  ,x*i)
+#     i+=1
+
+# nums = [1,2,4,9,16,25,36,49,64,81,100]
+# idx=0
+# while idx<len(nums):
+#     print(nums[idx])
+#     idx+=1
+
+# nums = (1,2,4,9,16,25,36,49,64,81,100)
+# x=25
+# i=0
+# while i< len(nums):
+#     if(nums[i] == x):
+#         print("found",i)
+#         break
+#     i+=1
+
+# i=0
+# while i<=10:
+#     if(i%2==0):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+
+#for loop
+# list = [1,3,4,5,6]
+# for i in list:
+#     print(i)
+
+# tup = ("falsi")
+# for char in tup:
+#     print(char)
+
+#practice 
+# nums = [1,2,4,9,16,25,36,49,64,81,100]
+# for i in nums:
+#     print(i)
+
+# nums = (1,2,4,9,16,25,36,49,64,81,100,25)
+# x=25
+# for i in nums:
+#     if(i == x):
+#         print("found",i)
+    # i+=1
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(5,10):
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+
+#practice
+# n = 7
+# sum=0
+# for i in range(n):
+#     sum += i
+# print(sum)
