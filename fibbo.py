443eb3b85753df52d7f8fbd7e81d99a0697b580f
Vadaliya Falsi <110164717+Falsi3007@users.noreply.github.com>
2025-01-20 12:24:19 +0530

    Create fibbo.py

---
+++ fibbo.py
@@ -0,0 +1,26 @@
+# 1 1 2 3 5 8 13 
+
+#addtion of fibbo series
+# def fibbo(n):
+#     if n <= 0: 
+#         return 0
+#     elif n==1 or n==2:
+#         return 1
+#     else:
+#         return fibbo(n-1)+fibbo(n-2)
+
+# print(fibbo(2))
+
+
+#print the series 
+# def fibbo(n):
+#     if n <= 0: 
+#         return 0
+#     elif n==1 or n==2:
+#         return 1
+#     else:
+#         return fibbo(n-1)+fibbo(n-2)
+
+# n = int(input("enter the no.:"))
+# for i in range(1,n+1):
+#     print(fibbo(i),end=" ")
